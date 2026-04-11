11650fe51e79077a197095eb9b041284ef24d344
TheFirym <thefirymsv@gmail.com>
2026-04-11 15:19:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -52,19 +52,6 @@ def get_player_list():
         logger.error(f"Ошибка при получении списка игроков: {e}")
         return "Не удалось получить список игроков."
 
-# Получение версии сервера
-def get_server_version():
-    try:
-        response = requests.get(f"https://api.mcsrvstat.us/2/{SERVER_IP}:{SERVER_PORT}")
-        data = response.json()
-        if data.get('online'):
-            return f"Версия сервера: {data.get('version', 'неизвестно')}"
-        else:
-            return "Не удалось получить версию сервера."
-    except Exception as e:
-        logger.error(f"Ошибка при получении версии сервера: {e}")
-        return "Не удалось получить версию сервера."
-
 # Команда проверки статуса сервера
 async def status(update: Update, context: CallbackContext):
     log_user(update)
@@ -74,11 +61,6 @@ async def status(update: Update, context: CallbackContext):
 async def players(update: Update, context: CallbackContext):
     log_user(update)
     await update.message.reply_text(get_player_list())
-
-# Команда получения версии сервера
-async def server_version(update: Update, context: CallbackContext):
-    log_user(update)
-    await update.message.reply_text(get_server_version())
 
 # Команда старт
 async def start_command(update: Update, context: CallbackContext):
@@ -96,53 +78,13 @@ async def start_command(update: Update, context: CallbackContext):
     )
     await update.message.reply_text(start_message)
 
-# Команда получения списка админов и модераторов
-async def staff(update: Update, context: CallbackContext):
-    log_user(update)
-    staff_list = {
-        "Админ": ["Cymniki(@CKirilll)"],
-         "Не Модер": ["KILLAH"]
-       }
-    response = "\n".join([f"{role}: {', '.join(names)}" for role, names in staff_list.items()])
-    await update.message.reply_text(f"Состав сервера:\n{response}")
-
-# Команда получения IP сервера
-async def server_ip(update: Update, context: CallbackContext):
-    log_user(update)
-    await update.message.reply_text("IP сервера: thefirym.gomc.fun")
-
-# Команда для присоединения
-async def join(update: Update, context: CallbackContext):
-    log_user(update)
-    join_message = "КАНАЛ ДЛЯ ЗАЯВКИ: https://t.me/+CpN-PsMRvuExMzRi"
-    await update.message.reply_text(join_message)
-
-# Команда для правил
-async def rules(update: Update, context: CallbackContext):
-    log_user(update)
-    rules_message = (
-        "Правила сервера:\n"
-        "1. Не ломать чужие постройки\n"
-        "2. Не грабить игроков\n"
-        "3. Не убивать домашних животных, которые принадлежат не вам\n"
-        "4. Запрещается быть без мода Emotecraft и Simple Voice Chat 💬\n"
-        "5. Уважать остальных участников сервера\n"
-        "6. У каждого игрока должен быть свой скин"
-    )
-    await update.message.reply_text(rules_message)
-
 # Создание приложения бота
 app = Application.builder().token(TOKEN).build()
 
 # Добавление команд
 app.add_handler(CommandHandler("status", status))
 app.add_handler(CommandHandler("online", players))
-app.add_handler(CommandHandler("players", staff))
-app.add_handler(CommandHandler("ip", server_ip))
-app.add_handler(CommandHandler("version", server_version))
 app.add_handler(CommandHandler("start", start_command))
-app.add_handler(CommandHandler("join", join))
-app.add_handler(CommandHandler("rules", rules))
 
 if __name__ == "__main__":
     try:
